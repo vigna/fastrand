bce595ad31ab1342678606105609dd6a2ab86f68
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2026-05-12 09:38:33 +0200

    Neutral u64 tests

--- src/lib.rs
+++ src/lib.rs
@@ -146,7 +146,7 @@ impl Rng {
 
     /// Generates a random `u64`.
     #[inline]
-    fn gen_u64(&mut self) -> u64 {
+    pub fn gen_u64(&mut self) -> u64 {
         let result = self.0;
         self.0 = self.0.wrapping_add(1);
         result
